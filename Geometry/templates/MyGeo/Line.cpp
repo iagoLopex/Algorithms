366577390fdbc9e9b713e410c74a5ba66d49e4f6
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-07-10 10:17:15 -0300

    Create Line.cpp

---
+++ Geometry/templates/MyGeo/Line.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define eps 1e-9
+#define pi acos(-1.0)
+
+using namespace std;
+
+template<class F>
+struct Point{
+	F x, y;
+	Point(): x(0), y(0){};
+	Point(F a, F b): x(a), y(b){};
+	
+	bool operator < (const Point &u) const{ if(fabs(x-u.x) < eps){ return y < u.y; }else{ return x < u.x; } }
+	bool operator > (const Point &u) const{ if(fabs(x-u.x) < eps){ return y > u.y; }else{ return x > u.x; } }
+	bool operator == (const Point &u) const{ return (fabs(x-u.x) < eps && fabs(y-u.y) < eps); }
+	bool operator = (const Point &u){ x=u.x; y=u.y; return true; }
+	
+	friend istream & operator >>(istream &in, Point &u){ return in >> u.x >> u.y; }
+	friend ostream & operator <<(ostream &out, Point &u){ return out << " P(" << u.x << "," << u.y << ") "; }
+};
+
+//-----------------------------------------------start----------------------------------------------------------//
+
+/*
+ * Note:
+ * if A==0, parallel to x, passing to C
+ * if B==0, parallel to y, passing to -(C/A)
+ * if C==0, pass to Point(0,0)
+ */
+
+template<class F>
+class line{
+public:
+	double A, B, C;
+	double a, b;
+	double num, den;
+	
+	line(){A = a = B = b = C = 0; }
+	line(F nume, F deno, Point<F> &v){
+		A = deno, B = -nume, C = -(deno * v.x) + (nume * v.y);
+		a = (fabs(B)<eps ? 1.0 : -A/B), b = (fabs(B)<eps ? 0 : -C/B);
+	}
+	line(Point<F> &u, Point<F> &v){
+		den = u.x - v.x;
+		num = u.y - v.y;
+		A = num, B = -den, C = -(num * u.x) + (den * u.y);
+		den*=-1;
+		a = (fabs(B)<eps ? 1.0 : -A/B), b = (fabs(B)<eps ? 0 : -C/B);
+	}
+	~line(){};
+	friend ostream & operator <<(ostream &out, const line &u){ return out << u.a << "x " << u.b << "\n" << u.A << "x " << u.B << "y " << u.C << " = 0"; }
+};
+
+//-----------------------------------------------finish----------------------------------------------------------//
+int main(){
+}
