cfd7e8705fa08ce283eb75573c998ea399537fe8
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-07-10 10:18:38 -0300

    Delete Angle.cpp

--- Geometry/templates/Angle.cpp
+++
@@ -1,7 +0,0 @@
-float ang(const Point<double> &a, const Point<double> &b, const Point<double> &c){//BAO
-	double d1, d2, d3;
-	d1=dist(a, b); d2=dist(a, c); d3=dist(b, c);
-	float h = ((d1*d1)+(d2*d2)-(d3*d3))/(2*d1*d2);
- 	float k = acos(h)*180/pi;
-	return k;
-}
