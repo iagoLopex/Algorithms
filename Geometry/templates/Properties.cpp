32626336fa75cfefa82e63f730a71d931f75b10b
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-07-12 16:41:36 -0300

    Update Properties.cpp

--- Geometry/templates/Properties.cpp
+++ Geometry/templates/Properties.cpp
@@ -1,18 +1,16 @@
 //HERON
 peri=(a+b+c)/2
-A = sqrt(peri*(peri-a)*(peri-b)*(peri-c))/2
+A = sqrt(peri*(peri-a)*(peri-b)*(peri-c))
 
 //BARICENTER
 //the proportion of median is 1/3 + 2/3, of all medians
 //the proportion of area is 1/6
 //with medians(a, b, c) is possible to calculate the area with:
 peri = (a+b+c)/2
-A = 4/3*(sqrt(peri*(peri-a)*(peri-b)*(peri-c))/2)
+A = 4/3*(sqrt(peri*(peri-a)*(peri-b)*(peri-c)))
 
 //CENTROID TRIANGLE
 x=(x1+x2+x3)/3, y=(y1+y2+y3)/3
 
 //CENTROD QUADRILATERAL
 x=(x1+x2+x3+x4)/4, y=(y1+y2+y3+y4)/4
-
-  
