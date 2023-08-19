380226a381825db0bebafb3db35ab23cd2bd7c8c
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-08-19 17:25:06 -0300

    Update PolygonArea.cpp

--- Geometry/templates/MyGeo/PolygonArea.cpp
+++ Geometry/templates/MyGeo/PolygonArea.cpp
@@ -1,4 +1,4 @@
-int area(const vector<pt>&vec){
+int area(const vector<Point>&vec){
 	int ans=0, n = (int)vec.size();
 	for(int i=0; i<n; i++){ ans += ((vec[i].x*vec[(i+1)%n].y) - (vec[(i+1)%n].x*vec[i].y)); }
 	return abs(ans)/2;
