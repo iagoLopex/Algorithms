d84da4dc75fdf53beafa7167e5d9680178aead83
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-10-17 13:05:40 -0300

    Update Point.cpp

--- Geometry/templates/MyGeo/Point.cpp
+++ Geometry/templates/MyGeo/Point.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define pi acos(-1.0)
-#define EPS -1e9
+#define ld long double
+
+const long double EPS = 1e-7;
+
 
 //-----------------------------------------------start----------------------------------------------------------//
 
@@ -16,13 +17,17 @@ struct Point{
 	bool operator < (const Point &u) const{ if(fabs(x-u.x) < EPS){ return y < u.y; }else{ return x < u.x; } }
 	bool operator > (const Point &u) const{ if(fabs(x-u.x) < EPS){ return y > u.y; }else{ return x > u.x; } }
 	bool operator == (const Point &u) const{ return (fabs(x-u.x) < EPS && fabs(y-u.y) < EPS); }
-	
+	Point operator - (const Point& b) const { return Point{x - b.x, y - b.y}; }
+        F operator ^(const Point& b) const { return x*b.y - y*b.x; } //neg(direita) , pos(esquerda), 0(colinear), usa um vetor base(p1 - p2) ^ (pn - pn+1); 
+    
+        F dist(Point b) { return sqrt((x-b.x)*(x-b.x) + (y-b.y)*(y-b.y)); }
+    
 	friend istream & operator >>(istream &in, Point &u){ return in >> u.x >> u.y; }
 	friend ostream & operator <<(ostream &out, Point &u){ return out << " P(" << u.x << "," << u.y << ") "; }
 };
 
 //-----------------------------------------------finish----------------------------------------------------------//
 
-int main(){
-	
+
+int main() {
 }
